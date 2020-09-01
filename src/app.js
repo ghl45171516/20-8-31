b297b6d6317c6e63ab5916c6ff19fb5283590443
Mr.Guo <ghl45171516@163.com>
2020-09-01 13:16:47 +0800

    提交

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
-const users = require('./module/user.js')
+const users = require('./routes/user.js')
 
 // 增加
 app.use('/users', users)
+
+
+
+
+
+
+
 
 app.listen(3000)
 console.log('服务器连接成功 访问：http://localhost:3000')
